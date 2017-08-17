cfd9f905c3c376d9fa256e4f94cd98ceb9e02617
lvzu1996 <385162733@qq.com>
2017-08-17 12:21:28 +0800

    add first app

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,28 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
-
 # Create your views here.
+@require_http_methods(["GET"])
+def add_book(request):
+    response = {}
+    try:
+        book = Book(book_name=request.GET.get('book_name'))
+        book.save()
+        response['msg'] = 'success'
+        response['error_num'] = 0
+    except  Exception,e:
+        response['msg'] = str(e)
+        response['error_num'] = 1
+
+    return JsonResponse(response)
+
+@require_http_methods(["GET"])
+def show_books(request):
+    response = {}
+    try:
+        books = Book.objects.filter()
+        response['list']  = json.loads(serializers.serialize("json", books))
+        response['msg'] = 'success'
+        response['error_num'] = 0
+    except  Exception,e:
+        response['msg'] = str(e)
+        response['error_num'] = 1
+
+    return JsonResponse(response)
