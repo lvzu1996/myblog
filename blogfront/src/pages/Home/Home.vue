634f45547e181e2c590085c99bde2aad737ec6f6
lvzu1996 <385162733@qq.com>
2017-09-04 18:31:54 +0800

    在主界面增加了访问设备识别，移动设备访问跳转至about-me

--- blogfront/src/pages/Home/Home.vue
+++ blogfront/src/pages/Home/Home.vue
@@ -90,6 +90,26 @@ export default {
   },
 
   mounted: function() {
+
+
+    var sUserAgent = navigator.userAgent.toLowerCase();
+    var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+    var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+    var bIsAndroid = sUserAgent.match(/android/i) == "android";
+    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+
+    if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+          t.$router.push({ path:'/about-me' })
+    } else {
+      console.log('pc');
+    }
+
+
+
     // localStorage.isVisited = false;
     const t = this
     setInterval(function () {
